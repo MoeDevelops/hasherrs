c76c972b27467098a018a292e36a853b71f3a1e5
MoeDevelops <niesenmoritz@gmail.com>
2024-06-09 01:34:10 +0200

    Changed Argon2 library

--- src/routes/argon2.rs
+++ src/routes/argon2.rs
@@ -1,8 +1,6 @@
-use argon2::{
-    password_hash::{rand_core::OsRng, PasswordHasher, SaltString},
-    Argon2, Params,
-};
+use argon2_kdf::{Algorithm, Hasher};
 use axum::{http::StatusCode, Json};
+use base64::Engine;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -10,56 +8,50 @@ pub struct Argon2idParameters {
     algorithm: Option<String>,
     password: String,
     salt: Option<String>,
-    version: u8,
     parallelism: u32,
     memory: u32,
     iterations: u32,
-    hash_length: Option<usize>,
+    hash_length: Option<u32>,
 }
 
 pub async fn hash(
     Json(parameters): Json<Argon2idParameters>,
 ) -> Result<(StatusCode, String), (StatusCode, String)> {
-    let algorithm = match parameters.algorithm.as_deref() {
-        Some("i") => Ok(argon2::Algorithm::Argon2i),
-        Some("d") => Ok(argon2::Algorithm::Argon2d),
-        Some("id") => Ok(argon2::Algorithm::Argon2id),
-        None => Ok(argon2::Algorithm::Argon2id),
+    let mut argon2: Hasher = Hasher::new()
+        .memory_cost_kib(parameters.memory)
+        .iterations(parameters.iterations)
+        .threads(parameters.parallelism);
+
+    argon2 = match parameters.algorithm.as_deref() {
+        Some("i") => Ok(argon2.algorithm(Algorithm::Argon2i)),
+        Some("d") => Ok(argon2.algorithm(Algorithm::Argon2d)),
+        Some("id") => Ok(argon2.algorithm(Algorithm::Argon2id)),
+        None => Ok(argon2.algorithm(Algorithm::Argon2id)),
         _ => Err((
             StatusCode::UNPROCESSABLE_ENTITY,
             "Invalid algorithm".to_string(),
         )),
     }?;
 
-    let version = match parameters.version {
-        16 => Ok(argon2::Version::V0x10),
-        19 => Ok(argon2::Version::V0x13),
-        _ => Err((
-            StatusCode::UNPROCESSABLE_ENTITY,
-            "Invalid version".to_string(),
-        )),
-    }?;
+    argon2 = match parameters.hash_length {
+        Some(length) => argon2.hash_length(length),
+        _ => argon2,
+    };
 
     let salt = match parameters.salt {
-        Some(salt_parameter) => SaltString::from_b64(&salt_parameter)
-            .map_err(|e| (StatusCode::UNPROCESSABLE_ENTITY, e.to_string())),
-        _ => Ok(SaltString::generate(&mut OsRng)),
-    }?;
+        Some(ref salt_parameter) => base64::prelude::BASE64_STANDARD
+            .decode(salt_parameter)
+            .map_err(|e| (StatusCode::UNPROCESSABLE_ENTITY, e.to_string()))?,
+        _ => Vec::new(),
+    };
 
-    let argon2 = Argon2::new(
-        algorithm,
-        version,
-        Params::new(
-            parameters.memory,
-            parameters.iterations,
-            parameters.parallelism,
-            parameters.hash_length,
-        )
-        .map_err(|e| (StatusCode::UNPROCESSABLE_ENTITY, e.to_string()))?,
-    );
+    argon2 = match parameters.salt {
+        Some(_) => argon2.custom_salt(&salt),
+        _ => argon2,
+    };
 
     let password_hash = argon2
-        .hash_password(parameters.password.as_bytes(), &salt)
+        .hash(parameters.password.as_bytes())
         .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?
         .to_string();
 
